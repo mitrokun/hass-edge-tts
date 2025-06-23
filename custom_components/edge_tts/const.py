1d63af3d7fc5d6a3dfca333b31609493a6d4a528
mitrokun <108380896+mitrokun@users.noreply.github.com>
2025-06-23 20:41:24 +0300

    Update const.py

--- custom_components/edge_tts/const.py
+++ custom_components/edge_tts/const.py
@@ -2,6 +2,14 @@
 
 DOMAIN = "edge_tts"
 
+# Константы для опций
 CONF_LANG = "language"
+CONF_RATE = "rate"
+CONF_VOLUME = "volume"
+CONF_PITCH = "pitch"
 
+# Значения по умолчанию
 DEFAULT_LANG = "ru-RU"
+DEFAULT_RATE = "+0%"
+DEFAULT_VOLUME = "+0%"
+DEFAULT_PITCH = "+0Hz"
